d7276d007d600ab0f4304dd0cfa753648d23ac55
= <pacha.672@gmail.com>
2025-01-11 17:09:34 +0300

    Add file

--- 1.Circle Point Generator/PointsGenerate.py
+++ 1.Circle Point Generator/PointsGenerate.py
@@ -14,14 +14,14 @@ def generate2():
         if x ** 2 + y ** 2 > 1:
             return [x,y]
 with open("points.txt", "w", newline='') as file:
-    for cycle in range(10):
+    for cycle in range(100):
         nums = []
         rand = random.randint(1, 2)
         if rand == 1:
-            for _ in range(10):
+            for _ in range(1000):
                 nums.extend(generate1())
         elif rand == 2:
-            for _ in range(10):
+            for _ in range(1000):
                 nums.extend(generate2())
         nums_str = " ".join(map(str, nums))
         file.write(nums_str + "\n")
